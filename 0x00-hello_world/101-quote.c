edd66e101562e31dc0605067ffafa32fc88b390e
IbetheRamada <valerdagd@gmail.com>
2021-06-19 22:19:57 -0500

    fix the file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-
+#define STRING "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 /**
- * main - prints a text
- * 
- *  Return: Always 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
-    return(1);
+	int len = strlen(STRING);
 
+	write(1, STRING, len);
+	return (1);
 }
