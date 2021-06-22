2a3016c50e8883d6ee35b05443fcb96730944ffd
IbetheRamada <valerdagd@gmail.com>
2021-06-22 12:35:37 -0500

    fix the lines

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 /**
  *main - assign a number to the variable n each time it is executed 
- * If the number is greater than 0, it is positive. If the number is less than 0, it is negative.
+ * If the number is greater than 0, it is positive.
+ * If the number is less than 0, it is negative.
  * equal to 0, is zero.
  * Return: (0)
  */
