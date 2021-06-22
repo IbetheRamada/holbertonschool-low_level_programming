0191617917f4ef7b0be94b1f876b5c801abc6866
IbetheRamada <valerdagd@gmail.com>
2021-06-22 18:15:35 -0500

    add and fix the files 0 and 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,23 @@
-#include <stdlib.h>  
-#include <time.h>  
-#include <stdio.h>  
-/**  
-*main - assign a number to the variable n each time it is executed   
-* If the number is greater than 0, it is positive.  
-* If the number is less than 0, it is negative.  
-* equal to 0, is zero.  
-* Return: (0)  
-*/
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: 0 Always  (Success)
+ */
 int main(void)
-{  
-		int n;  
-	
-		srand(time(0));  
+{
+		int n;
 
-		n = rand() - RAND_MAX / 2;  
-		if (n > 0)  
-			printf("%d is positive\n", n);  
-		else if (n < 0)  
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+
+		if (n > 0)
+			printf("%d is positive\n", n);
+		else if (n < 0)
 			printf("%d is negative\n", n);  
-		else  
-			printf("%d is zero\n", n);  
+		else
+			printf("%d is zero\n", n);
 		return (0);
-}  
+}
