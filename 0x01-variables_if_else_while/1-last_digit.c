fd055c117ba38e4cf1ccb14a1e3b4da2704760c3
IbetheRamada <valerdagd@gmail.com>
2021-06-23 14:27:12 -0500

    fix the file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,21 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - the function main be  generates a random number  a validate
-*
-* Return: Always 0 (Sucess)
+*main - assign a number to the variable n each time it is executed
+* If the number is greater than 0, it is positive.
+* If the number is less than 0, it is negative.
+* equal to 0, is zero.
+* Return: (0)
 */
 int main(void)
 {
-	int n;
+		int n;
+		srand(time(0));
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-	return (0);
+		n = rand() - RAND_MAX / 2;
+		if (n > 0)
+			printf("%d is positive\n", n);
+		else if (n < 0)
+			printf("%d is negative\n", n);
+		else
+			printf("%d is zero\n", n);
+		return (0);
 }
