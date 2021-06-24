3b202d151fc34838d621623de86dc75b7ebccb64
IbetheRamada <valerdagd@gmail.com>
2021-06-24 15:46:44 -0500

    add the ; :D

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,31 +7,31 @@
  */
 int main(void)
 {  
-	putchar('a')
-	putchar('b')
-	putchar('c')
-	putchar('d')
-	putchar('e')
-	putchar('f')
-	putchar('g')
-	putchar('h')
-	putchar('i')
-	putchar('j')
-	putchar('k')
-	putchar('l')
-	putchar('m')
-	putchar('n')
-	putchar('o')
-	putchar('p')
-	putchar('q')
-	putchar('r')
-	putchar('s')
-	putchar('t')
-	putchar('u')
-	putchar('v')
-	putchar('w')
-	putchar('x')
-	putchar('y')
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y'):
 	putchar('z');
 	return (0);
 }
