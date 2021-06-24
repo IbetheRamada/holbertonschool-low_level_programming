3d0d3fce59382fc0e863c0f407df3675ee565ab2
IbetheRamada <valerdagd@gmail.com>
2021-06-24 15:32:33 -0500

    Fix all the files :D

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{  
+    putchar('abcdefghijklmnopqrstuvwxyz');
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/testing.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void)
+{
+	prtinf("Hello my friends\n");
+	return (0);
+}
