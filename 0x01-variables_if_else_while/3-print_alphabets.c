194ea7c2675a24aab1a53a73753de3adf3e26cec
IbetheRamada <valerdagd@gmail.com>
2021-06-24 23:41:55 -0500

    fix the file :D

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; c <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	for (letter = 'A'; c <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar ('\n');
 	return (0);
