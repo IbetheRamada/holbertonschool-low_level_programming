fb0d6855d01e5f355e23b10a1e676eb5ffc7ce8e
IbetheRamada <valerdagd@gmail.com>
2021-06-26 13:52:37 -0500

    add the 5 file :D

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 10 to 0
+ *
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+	char number = 47;
+
+	while (number < 58)
+	{
+		number = number + 1;
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
