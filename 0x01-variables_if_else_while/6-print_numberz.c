f4536ca0513091fcb294539facdb845ab917d9bb
IbetheRamada <valerdagd@gmail.com>
2021-06-26 20:38:09 -0500

    add the 6 file :D

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 10 to 0 without char variable.
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = -1;
+	while (number <= 10)
+	{
+		Number = number + 1;
+		putchar(number)
+	}
+	putchar("\n")
+	return (0);
+}
