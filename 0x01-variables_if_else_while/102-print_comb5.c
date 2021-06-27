f3d21a822df59d7be710b67e17150bdb93c909dd
IbetheRamada <valerdagd@gmail.com>
2021-06-27 02:59:53 -0500

    add the 102 file :D

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers combination of two digit separated by ,
+ * and following by space.
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int var_1;
+	int var_2;
+	int var_3;
+	int var_4;
+
+	for (var_1 = 48; var_1 <= 57 ; var_1++)
+	{
+		for (var_2 = var_1; var_2 <= 57; var_2++)
+		{
+			for (var_3 = var_2; var_3 <= 57; var_3++)
+
+			{
+				for (var_4 = var_3; var_4 <= 57; var_4++)
+
+				{
+				putchar(var_1);
+				putchar(var_2);
+				putchar(32);
+				putchar(var_3);
+				putchar(var_4);
+				if (var_1 < 57)
+					{
+					putchar(44);
+					putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
