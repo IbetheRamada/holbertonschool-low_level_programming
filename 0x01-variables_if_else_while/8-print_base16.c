ef762554e7496fb97ad37ca3c09eec61947e63dd
IbetheRamada <valerdagd@gmail.com>
2021-06-26 23:50:09 -0500

    add the 8 file :D

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in reverse.
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int car;
+
+	car = 48;
+	while (car < 103)
+	{
+		if (car == 58)
+		car = 96;
+		putchar(car);
+		car++;
+	}
+	return (0);
+}
+
