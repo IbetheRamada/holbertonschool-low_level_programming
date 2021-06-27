bbf8dc57a5b280ec51cfadd318c4f5e0d409f64d
IbetheRamada <valerdagd@gmail.com>
2021-06-27 02:28:04 -0500

    add the 101 file :D

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers combination of two digit separated by ,
+ * and following by space.
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int var_1;
+	int var_2;
+	int var_3;
+
+	for (var_1 = 48; var_1 <= 55 ; var_1++)
+	{
+		for (var_2 = var_1 + 1; var_2 <= 56; var_2++)
+		{
+			for(var_3 = var_2 + 1; var_3 <= 57; var_3++)
+			{
+			putchar(var_1);
+			putchar(var_2);
+			putchar(var_3);
+			if (var_1 < 55)
+			{
+			putchar(44);
+			putchar(32);
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
