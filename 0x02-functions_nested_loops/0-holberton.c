007edb2a545b7773efea5236d8603f54390d481a
IbetheRamada <valerdagd@gmail.com>
2021-06-27 19:16:46 -0500

    add the 0x02 directory andd 0 file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include <holberton.h>
+
+/**
+*main - assign a number to the variable n each time it is executed
+* If the number is greater than 0, it is positive.
+* If the number is less than 0, it is negative.
+* equal to 0, is zero.
+* Return: (0)
+*/
+
+int main(void)
+{
+	char var[] = "holberton";
+	int var_1;
+
+	var_1 = 0;
+	while (var_1 <= 8)
+	{
+		_putchar(var[var_1]);
+		var_1++;
+	}
+	_putchar('\n');
+	return (0);
+}
