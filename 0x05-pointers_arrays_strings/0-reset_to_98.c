9b6ade5cf7a346fbe0c3ffba427924c00a74d5f4
IbetheRamada <valerdagd@gmail.com>
2021-06-28 22:20:24 -0500

    add the stdio header

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int n;
-	int *reset_to_98[1];
+	
 	
 	n = 402;
 	printf("n=%d\n", n);
-	reset_to_98[0] = &n;
+	int *reset_to_98[1] = &n;
 	n = 98;
 	printf("n=%d\n", n);
 	return (0);
