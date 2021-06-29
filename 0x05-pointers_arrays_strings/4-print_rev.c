22d8e17553fd5cb7b089cf1e1cef0af0f94ef3ba
IbetheRamada <valerdagd@gmail.com>
2021-06-29 03:10:52 -0500

    add the 4 file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*
+*
+* Return: Always (0) (Success)
+*/
+void print_rev(char *s);
+{
+	int a = strlen (s);
+	int y = 0;
+	for (y = a; y > 0; y++)
+	{
+		printf("%d", *s[y]);
+	}
+	putchar('\n');
+}
