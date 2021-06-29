7bba4c0e12b57144484613d37bc0f0812da73b35
IbetheRamada <valerdagd@gmail.com>
2021-06-29 00:44:32 -0500

    fix the spaces

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strlen - function to return the length of a string.
+*@str: variable to print a string
+* Return: Always (text) (Success)
+*/
+void _puts(char *str);
+{
+	char text = (str);
+	return (text);
+}
