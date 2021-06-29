cf05a0dbebd8d2eaad5836be8cfbafb9f6667e3c
IbetheRamada <valerdagd@gmail.com>
2021-06-29 00:02:34 -0500

    add the 1 file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*_strlen - function to return the length of a string.
+*@s 
+* Return: (0)
+*/
+int _strlen(char *s);
+{
+	int longitud = strlen (*s);	
+}
