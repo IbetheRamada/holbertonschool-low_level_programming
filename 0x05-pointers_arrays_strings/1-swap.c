e73a6528b2a78420aab5ed098c1da0c0f614ec8b
IbetheRamada <valerdagd@gmail.com>
2021-06-28 23:34:15 -0500

    add the 1 file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*swap_int - Function to swap two values
+*@a: first value to change
+*@b: second value to change
+* Return: (0)
+*/
+void swap_int(int *a, int *b);
+{
+	*a = *b;
+	*b = c;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,12 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+void reset_to_98(int *n);
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int ab);
-int print_last_digit(int ld);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif
