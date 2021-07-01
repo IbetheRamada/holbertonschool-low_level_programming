3415b3b8bdbd177cb287a5b9c73b3093cbc7e11e
IbetheRamada <valerdagd@gmail.com>
2021-07-01 18:38:20 -0500

    add the 5 file

---
+++ 0x05-pointers_arrays_strings/5-rev_strinf.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*rev_string - print in reverse one string
+*@s: variable of string
+* Return: Always (p) (Success)
+*/
+void rev_string(char *s)
+{
+	int tam = 1;
+	int cont1 = 0;
+	int y = 0;
+
+	while (tam > 0)
+	{
+	if (s[cont1] == '\0')
+	break;
+	cont1++;
+	}
+
+
+	for (y = (cont1 - 1); y >= 0; y--)
+	{
+
+	printf("%c", s[y]);
+
+
+	}
+	putchar ('\n');
+}
