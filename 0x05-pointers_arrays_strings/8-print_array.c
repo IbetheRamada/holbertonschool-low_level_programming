26945f2dd42430480201c1bd752bfeef60c8356c
IbetheRamada <valerdagd@gmail.com>
2021-07-05 21:42:02 -0500

    fix the 6 file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*print_array: prints a array
+*@a: variable pointer
+*@n: variable int
+* Return: Always (p) (Success)
+*/
+void print_array(int *a, int n)
+{
+	int y = 0;
+	int cont = n;
+	cont--;
+	for(y = 0; y <= cont; y++)
+	{
+		printf("%d", a[y]);
+		if(y < cont)
+		{
+		putchar(44);
+		putchar(32);
+		}
+	}
+}
