9533e8c25c515e9ed03ca659e39719a0ef7fd664
IbetheRamada <valerdagd@gmail.com>
2021-07-07 01:48:48 -0500

    add the 1 file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+*_memcpy - copy a memory area
+*@dest: destiny variable 
+*@src: vairiable that have a string
+*@n: bytes
+*
+*Return: Always (dest) (success)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	dest = src[n];
+	return (dest);
+}
