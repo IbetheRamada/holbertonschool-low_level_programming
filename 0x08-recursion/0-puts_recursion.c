ea6117b05f4c227ef360d8c916a544afb3517884
IbetheRamada <valerdagd@gmail.com>
2021-07-08 14:33:02 -0500

    add the 0file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,10 @@
 */
 void _puts_recursion(char *s)
 {
-	int cont = 0;
-
-	if (s[cont] != '\0')
-	putchar(s[cont]);
-	cont++;
-
+	if (*s == '\0')
+	{
+		return('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
