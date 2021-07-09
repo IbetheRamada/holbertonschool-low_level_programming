15edeb469046fb74473830b337659d9157020a68
IbetheRamada <valerdagd@gmail.com>
2021-07-08 22:51:09 -0500

    add the 2 file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*/
+int factorial(int n);
+{
+	int a = 2;
+
+	if ((a%n) < 0)
+	{
+		return(-1);	
+	}
+	if ((a%n) = 0)
+	{
+		return(1);
+	}
+	if ((a%n) > 0)
+	{
+		n = n/a;
+	}
+	a++
+}
