03a84fd0d9d0e76a1ca6e8c0bbf0ef91a153bba6
IbetheRamada <valerdagd@gmail.com>
2021-07-08 21:57:34 -0500

    add the 2 file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
+* _strlen_recursion - print a leng of a string
 *
-*
-*
+*@s: variable that have a string
 */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
-	return(1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
