e931bf9bc9f72deff6352455c1f215b1f995cfef
IbetheRamada <valerdagd@gmail.com>
2021-07-08 20:32:26 -0500

    add the 1 file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-*
+*_print_rev_recursion - print a string in reverse
 *
 *
 *
@@ -10,7 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
+		_print_rev_recursion(++s);
 		putchar(*s);
 	}
 }
