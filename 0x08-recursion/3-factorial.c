5e1794008ea9394d0ad4895bd8ad583f045001c0
IbetheRamada <valerdagd@gmail.com>
2021-07-09 11:31:57 -0500

    add the 3 file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,16 +4,17 @@
 * factorial - calculate the factorial of a number
 *
 *@n: numbers we have to factorize
+*  Return: Always (factorial) (success)
 */
 int factorial(int n)
 {
 	if (n < 0)
 	{
-		return(-1);	
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		return(1);
+		return (1);
 	}
 	return (n * factorial(n - 1));
 
