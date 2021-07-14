edf4800977dca44ed1cebdb3addbc5367a609933
IbetheRamada <valerdagd@gmail.com>
2021-07-13 23:54:00 -0500

    add the 0 file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *a;
 	unsigned int i = 0;
-	a = (char *) malloc((sizeof(int) * size));
+	a = malloc((sizeof(char) * size));
 	if (size == 0)
 		return (NULL);
 	if (a == 0)
