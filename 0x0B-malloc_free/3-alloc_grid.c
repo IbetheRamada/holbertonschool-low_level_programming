5c3c2c76349deee882b7cc9fe325f58cc6faf5f4
IbetheRamada <valerdagd@gmail.com>
2021-07-15 21:57:24 -0500

    add the 3 file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i <= (height - 1); i++)
+	for (i = 0; i <= height; i++)
 	{
 	array[i] = malloc((width  * sizeof(int)) + i);
 		if (array[i] == NULL)
