73f6a86563740e29f7ba20c211ef8d96848add3a
IbetheRamada <valerdagd@gmail.com>
2021-07-22 18:47:36 -0500

    Fix: add libraries

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,11 +9,13 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-		size_t i;
+	unsigned int i;
 
-	if (array != NULL && action != NULL)
+	if (array && action)
 	{
-		for (i = 0 ; i < size ; i++)
-			(*action)(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 	}
 }
+
+
