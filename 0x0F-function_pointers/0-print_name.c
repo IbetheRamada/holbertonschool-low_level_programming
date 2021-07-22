8331611cc9de46d8ff4da80614ef6ffa3f9157b8
IbetheRamada <valerdagd@gmail.com>
2021-07-21 22:21:42 -0500

    fix the file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include "function_pointers.h"
 /**
-*
-*
-*
-*
+* print_name - function what will print a name
+*@name: a char what guard a name
+*@f: a void
 */
 void print_name(char *name, void (*f)(char *))
 {
