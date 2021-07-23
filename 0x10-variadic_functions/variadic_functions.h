894b89e13b9125c211502774a004abb0ddc3463c
IbetheRamada <valerdagd@gmail.com>
2021-07-23 18:34:46 -0500

    add the 0x10 directory and 0 file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
 #ifdef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-int sum_them_all(const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...);
 
 #endif /*variadic_functions.h*/
