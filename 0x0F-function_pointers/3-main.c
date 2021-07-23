c19f24480731ac5ecf7cf90f4911587078220630
IbetheRamada <valerdagd@gmail.com>
2021-07-23 00:56:34 -0500

    add and fix all the 3 files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,23 +7,16 @@
  */
 int main(int argc, char *argv[])
 {
+	char *operator = argv[2];
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	char *operator = argv[2];
 
-	get_op_func(operator);
-
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
-
-	if (b == 0 && (*argv[2] == '%' || *argv[2] == '/'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
 
 	printf("%d\n", get_op_func(operator)(a, b));
 	return (0);
