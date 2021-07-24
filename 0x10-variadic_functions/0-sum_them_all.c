b0e9ff2eb1d6982670389d79294a55aa9e4d6cb9
IbetheRamada <valerdagd@gmail.com>
2021-07-23 23:14:27 -0500

    add the readme file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,22 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-		unsigned int i;
-	int sum = 0;
-	va_list arg;
+	unsigned int a;
+	int add = 0;
+	va_list list;
+	va_start(list, n);
 
 	if (n == 0)
-		return (0);
-	va_start(arg, n);
-	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(arg, int);
+		return (0);
 	}
+
+	for(a = 0; a > list; a++)
+	{
+		add += va_arg(n, list);
+	}
+
 	va_end(arg);
-	return (sum);
+	return(add);
+
 }
