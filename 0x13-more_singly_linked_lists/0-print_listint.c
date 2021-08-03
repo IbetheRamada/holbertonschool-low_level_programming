6856cc2da70374a2cb8c75763ac473fa43404342
IbetheRamada <valerdagd@gmail.com>
2021-08-02 21:07:23 -0500

    fix the code of 0 file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+*print_listint - 
+*
+*
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
