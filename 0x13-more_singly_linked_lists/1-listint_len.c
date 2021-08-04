def8e0c91d58436029e2ac0c134d229bd5454d2a
IbetheRamada <valerdagd@gmail.com>
2021-08-03 22:30:38 -0500

    fix the one file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,7 +10,7 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count;
 
-	for (count = 0; h; i++)
+	for (count = 0; h; count++)
 	{
 		h = h->next;
 	}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+*add_nodeint - Add a new node at the beginning of the list
+*@head: a listint_h list
+*@n: a const int
+*Return: Always(*head) (success)
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n);
