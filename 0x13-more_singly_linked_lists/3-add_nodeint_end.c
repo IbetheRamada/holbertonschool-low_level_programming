16f944aae2b24389789ba12dfd5e99b9ee44b979
IbetheRamada <valerdagd@gmail.com>
2021-08-04 09:51:36 -0500

    fix the one file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,9 +1,29 @@
 #include "lists.h"
 
 /**
-*add_nodeint - Add a new node at the beginning of the list
+*add_nodeint_end - Add a new node at the beginning of the list
 *@head: a listint_h list
 *@n: a const int
 *Return: Always(*head) (success)
 */
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *current;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
+	return (*head);
+}
